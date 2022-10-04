11e99e4ddae5ea5361042b2c284396d170e9cde7
Azeez Olasoji <azeezolasoji@gmail.com>
2022-10-04 18:13:57 +0100

    restyled css, making it neater

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,19 @@ import React from 'react';
 export default function Content(){
     return(
         <div>
-            <p>content</p>
+            <section className='content--section'>
+                <h3>About</h3>
+                <p>I am frontend developer with particular interest in making things
+                    simple and automating daily tasks. I try to keep up with security best 
+                    practices, and am always looking for new things to learn
+                </p>
+
+                <h3>Interests</h3>
+                <p>Food expert. Music scholar. Academic scholar. Avid reader. Internet Junkie.
+                    Meat buff. Enterpreneur. Travel geek. Anime fanatic. Coffee fanatic.
+                </p>
+            </section>
+            <section></section>
         </div>
     )
 }
